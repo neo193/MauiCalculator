8c26d229de0c17c43846bf882eeff2c0b25b1a54
Sriharsha KR <53250729+neo193@users.noreply.github.com>
2023-11-03 21:34:11 +0530

    Polishing changes. Supports Dynamic calculations (Not limited to 1 operation).

--- HelloMaui/View/CalcMainPage.xaml.cs
+++ HelloMaui/View/CalcMainPage.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 namespace HelloMaui.View;
 
 public partial class CalcMainPage : ContentPage
@@ -5,39 +6,68 @@ public partial class CalcMainPage : ContentPage
     string a = string.Empty;
     string b = string.Empty;
     string Operator = string.Empty;
+    double result;
 
     private void Number_Clicked(object sender, EventArgs e)
     {
         Button button = (Button)sender;
         b += button.Text;
-        ExpressionField.Text = b;
+        if(ExpressionField.Text == "0" || String.IsNullOrEmpty(ExpressionField.Text))
+        {
+            ExpressionField.Text = b;
+        }
+        else
+        {
+            ExpressionField.Text += button.Text;
+        }
+        if (!String.IsNullOrEmpty(a))
+        {
+            double num1 = Convert.ToDouble(a);
+            double num2 = Convert.ToDouble(b);
+            result = 0;
+            switch (Operator)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+            Answer.Text = result.ToString();
+            
+        }
     }
     private void Operator_Clicked(object sender, EventArgs e)
     {
         Button button = ( Button )sender;
         Operator = button.Text;
-        a = b;
-        b = string.Empty;
-
+        if (String.IsNullOrEmpty(a))
+        {
+            a = b;
+            b = string.Empty;
+            ExpressionField.Text += Operator;
+        }
+        else
+        {
+            a = result.ToString();
+            b = string.Empty;
+            ExpressionField.Text += Operator;
+        }
+        //a = b;
+        //b = string.Empty;
+        //ExpressionField.Text += Operator;
     }
     private void Equals_Clicked(object sender, EventArgs e)
     {
-        double num1 = Convert.ToDouble(a);
-        double num2 = Convert.ToDouble(b);
-        double result = 0;
-        switch(Operator)
-        {
-            case "+": result = num1 + num2; 
-                break;
-            case "-": result = num1 - num2;
-                break;
-            case "*": result = num1 * num2;
-                break;
-            case "/": result = num1 / num2;
-                break;
-        }
-        Answer.Text = result.ToString();
-        b = result.ToString();
+        Answer.Text = string.Empty;
+        ExpressionField.Text = result.ToString();
     }
     private void Reset_Clicked(object sender, EventArgs e)
     {
